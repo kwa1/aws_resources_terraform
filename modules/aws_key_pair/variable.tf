452ac15f5718dfcbbb248499081f8f3e75e82dc2
Dennis Amankwa <amankwa32@hotmail.com>
2024-12-29 21:51:15 +0000

    Update variable.tf

--- modules/aws_key_pair/variable.tf
+++ modules/aws_key_pair/variable.tf
@@ -1,1 +1,20 @@
-variable "key_name" {}
+# Name of the key pair
+variable "key_name" {
+  description = "Name of the EC2 key pair and associated resources."
+  type        = string
+}
+
+# RSA key size (2048 or 4096 bits)
+variable "rsa_bits" {
+  description = "Bit length of the RSA private key."
+  type        = number
+  default     = 4096
+}
+
+# Environment (e.g., dev, staging, prod)
+variable "environment" {
+  description = "Environment tag for resource identification."
+  type        = string
+  default     = "ea"
+}
+
