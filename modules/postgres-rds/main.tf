0babcffdc19648046cd754e23f132dd2760aade0
Dennis Amankwa <amankwa32@hotmail.com>
2024-12-30 22:59:00 +0000

    Create main.tf

---
+++ modules/postgres-rds/main.tf
@@ -0,0 +1,38 @@
+resource "aws_db_subnet_group" "this" {
+  name       = "${var.db_name}-subnet-group"
+  subnet_ids = var.subnet_ids
+  tags       = var.tags
+}
+
+resource "aws_db_instance" "this" {
+  allocated_storage      = var.allocated_storage
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  name                   = var.db_name
+  username               = var.username
+  password               = var.password
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  publicly_accessible    = var.publicly_accessible
+  storage_encrypted      = true
+  backup_retention_period = var.backup_retention_period
+  deletion_protection     = true
+  multi_az               = var.multi_az
+  tags                   = var.tags
+
+  monitoring_interval = 60
+  performance_insights_enabled = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "db_endpoint" {
+  value = aws_db_instance.this.endpoint
+}
+
+output "db_arn" {
+  value = aws_db_instance.this.arn
+}
