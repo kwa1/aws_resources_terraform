f7f4e4dd42b62ea7a39ba37bba500c6a3544f5b2
Dennis Amankwa <amankwa32@hotmail.com>
2024-12-30 22:19:45 +0000

    Update main.tf

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -3,6 +3,12 @@ resource "aws_ecr_repository" "this" {
   image_tag_mutability = var.image_tag_mutability
   encryption_configuration {
     encryption_type = var.encryption_type
+    kms_key         = var.kms_key_arn
+  }
+
+  # Enable repository scanning for vulnerabilities (good security practice)
+  image_scanning_configuration {
+    scan_on_push = true
   }
 
   tags = merge(var.tags, { Name = var.repository_name })
@@ -15,7 +21,7 @@ resource "aws_ecr_lifecycle_policy" "this" {
   depends_on      = [aws_ecr_repository.this]
 }
 
-# Optional IAM Policy for granting access to the ECR repository
+# IAM policy for repository access with least privilege
 resource "aws_iam_policy" "this" {
   name        = "${var.repository_name}-policy"
   description = "Policy to allow access to ECR repository ${var.repository_name}"
@@ -23,9 +29,28 @@ resource "aws_iam_policy" "this" {
   depends_on  = [aws_ecr_repository.this]
 }
 
-# Optional IAM Role or User association
+# Attach the IAM policy to the IAM role or user
 resource "aws_iam_role_policy_attachment" "this" {
   role       = var.iam_role
   policy_arn = aws_iam_policy.this.arn
   depends_on = [aws_ecr_repository.this]
+}
+
+# CloudWatch logging (for auditing)
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/ecr/repository/${var.repository_name}"
+  retention_in_days = 30
+}
+
+resource "aws_ecr_repository_event_configuration" "this" {
+  repository_name = aws_ecr_repository.this.name
+
+  event_configuration {
+    event_type = "PUSH"
+    destination {
+      cloudwatch_logs {
+        log_group = aws_cloudwatch_log_group.this.name
+      }
+    }
+  }
 }
