1967d57008e5e2931cac5651858f1e5d0ea4e95b
Dennis Amankwa <amankwa32@hotmail.com>
2024-12-31 00:30:25 +0000

    Update main.tf

--- modules/ses/main.tf
+++ modules/ses/main.tf
@@ -1,46 +1,50 @@
-# SES Domain Identity (if domain is provided)
+locals {
+  email_identity = "${var.user}@${var.email_domain}"
+}
+
+# SES Email Identity Resource
+resource "aws_ses_email_identity" "this" {
+  email = local.email_identity
+  tags  = var.tags
+}
+
+# SES Domain Identity (if email is set for the domain)
 resource "aws_ses_domain_identity" "this" {
-  count  = var.email_identity == null ? 1 : 0
-  domain = var.domain_name
+  count  = var.user == null ? 1 : 0
+  domain = var.email_domain
   tags   = var.tags
 }
 
-# SES Email Identity (if email_identity is provided)
-resource "aws_ses_email_identity" "this" {
-  count = var.email_identity != null ? 1 : 0
-  email = var.email_identity
-}
-
-# DKIM for Domain Identity
+# SES DKIM Records
 resource "aws_ses_domain_dkim" "this" {
-  count  = var.email_identity == null ? 1 : 0
+  count  = var.user == null ? 1 : 0
   domain = aws_ses_domain_identity.this[0].domain
 }
 
-# SES Domain Verification Token DNS Record (Optional)
+# Route 53 DNS Records for Email Identity Verification
 resource "aws_route53_record" "ses_verification" {
-  count   = var.manage_dns_records && var.email_identity == null ? 1 : 0
+  count   = var.manage_dns_records && var.user == null ? 1 : 0
   zone_id = var.route53_zone_id
-  name    = "_amazonses.${var.domain_name}"
+  name    = "_amazonses.${var.email_domain}"
   type    = "TXT"
   ttl     = 300
   records = [aws_ses_domain_identity.this[0].verification_token]
 }
 
-# DKIM CNAME Records for Domain (Optional)
+# Route 53 DKIM CNAME Records for Domain (Optional)
 resource "aws_route53_record" "dkim_records" {
-  count   = var.manage_dns_records && var.email_identity == null ? length(aws_ses_domain_dkim.this[0].dkim_tokens) : 0
+  count   = var.manage_dns_records && var.user == null ? length(aws_ses_domain_dkim.this[0].dkim_tokens) : 0
   zone_id = var.route53_zone_id
-  name    = "${element(aws_ses_domain_dkim.this[0].dkim_tokens, count.index)}._domainkey.${var.domain_name}"
+  name    = "${element(aws_ses_domain_dkim.this[0].dkim_tokens, count.index)}._domainkey.${var.email_domain}"
   type    = "CNAME"
   ttl     = 300
   records = ["${element(aws_ses_domain_dkim.this[0].dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
 
-# SES Domain Identity Verification
-resource "aws_ses_domain_identity_verification" "this" {
-  count  = var.email_identity == null ? 1 : 0
-  domain = aws_ses_domain_identity.this[0].domain
+# Optional - Email Identity Verification (for specific email)
+resource "aws_ses_email_identity_verification" "this" {
+  count  = var.user != null ? 1 : 0
+  email  = local.email_identity
 
   depends_on = [aws_route53_record.ses_verification]
 }
