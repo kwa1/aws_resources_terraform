e6ec8d3038b3c118bacc4b7ee54a1d5201432f61
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-01 14:14:22 +0000

    Create locals.tf

---
+++ modules/cognito/cognito_app_client/locals.tf
@@ -0,0 +1,6 @@
+# Use this file to calculate or define reusable local values if necessary.
+
+locals {
+  # Example local value for default OAuth scopes
+  default_oauth_scopes = ["openid", "email", "profile"]
+}
