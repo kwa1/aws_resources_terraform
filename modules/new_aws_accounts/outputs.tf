0172c06e65beb7acd03b157b5165783e48a75ab4
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-11 20:57:53 +0000

    Update outputs.tf

--- modules/new_aws_accounts/outputs.tf
+++ modules/new_aws_accounts/outputs.tf
@@ -1,4 +1,4 @@
-data "aws_caller_identity" "current" {}
+#data "aws_caller_identity" "current" {}
 
 output "account_id" {
   value       = aws_organizations_account.account.id
@@ -13,4 +13,18 @@ output "admin_role_arn" {
 output "read_only_role_arn" {
   value       = aws_iam_role.organization_role_read_only.arn
   description = "The ARN of the read-only role for the new AWS account"
+}
+output "add_roles_to_aws_config_file" {
+  value = <<EOF
+
+[profile ${aws_organizations_account.account.name}admin]
+role_arn = arn:aws:iam::${aws_organizations_account.account.id}:role/${aws_organizations_account.account.name}OrganizationRole
+source_profile = default
+regions = eu-west-1
+
+[profile ${aws_organizations_account.account.name}]
+role_arn = arn:aws:iam::${aws_organizations_account.account.id}:role/${aws_organizations_account.account.name}OrganizationRoleReadOnly
+source_profile = default
+regions = eu-west-1
+EOF
 }
