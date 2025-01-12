93d02eb1b6204775b4c1fb116bcefdc7703ee801
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-12 00:25:12 +0000

    Update outputs.tf

--- modules/cloudwatch-alarms/outputs.tf
+++ modules/cloudwatch-alarms/outputs.tf
@@ -1,3 +1,7 @@
 output "alarm_topic_arn" {
   value = aws_sns_topic.metric_alarms.arn
 }
+output "dashboard_name" {
+  value = aws_cloudwatch_dashboard.system_dashboard.dashboard_name
+  description = "Name of the CloudWatch dashboard for system metrics"
+}
