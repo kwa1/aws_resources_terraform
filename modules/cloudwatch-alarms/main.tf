b0426329e0be14fba2ca53276945f53904024c6d
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-12 00:17:43 +0000

    Update main.tf

--- modules/cloudwatch-alarms/main.tf
+++ modules/cloudwatch-alarms/main.tf
@@ -26,6 +26,62 @@ variable "metrics_alarm_config" {
       comparison_operator = string
     })
   )
+  default = [
+    {
+      name               = "HighCPUUtilization"
+      metric_name        = "cpu_usage_active"
+      threshold          = 80
+      evaluation_periods = 2
+      period             = 300
+      statistic          = "Average"
+      comparison_operator = "GreaterThanThreshold"
+    },
+    {
+      name               = "LowMemoryAvailable"
+      metric_name        = "mem_available_percent"
+      threshold          = 20
+      evaluation_periods = 2
+      period             = 300
+      statistic          = "Average"
+      comparison_operator = "LessThanThreshold"
+    },
+    {
+      name               = "HighDiskUsage"
+      metric_name        = "disk_used_percent"
+      threshold          = 90
+      evaluation_periods = 2
+      period             = 300
+      statistic          = "Average"
+      comparison_operator = "GreaterThanThreshold"
+    },
+    {
+      name               = "HighNetworkErrorRate"
+      metric_name        = "errors_out"
+      threshold          = 10
+      evaluation_periods = 2
+      period             = 300
+      statistic          = "Sum"
+      comparison_operator = "GreaterThanThreshold"
+    },
+    {
+      name               = "HighReadIO"
+      metric_name        = "read_bytes"
+      threshold          = 500000000  # 500 MB
+      evaluation_periods = 2
+      period             = 300
+      statistic          = "Sum"
+      comparison_operator = "GreaterThanThreshold"
+    },
+    {
+      name               = "HighWriteIO"
+      metric_name        = "write_bytes"
+      threshold          = 500000000  # 500 MB
+      evaluation_periods = 2
+      period             = 300
+      statistic          = "Sum"
+      comparison_operator = "GreaterThanThreshold"
+    }
+  ]
 }
 
 resource "aws_cloudwatch_metric_alarm" "metric_alarms" {
