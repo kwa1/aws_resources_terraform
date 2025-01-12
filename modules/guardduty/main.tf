09dd8c83d2d3aedf99d0074b7fe88a5783bedaf8
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-12 01:52:04 +0000

    Update main.tf

--- modules/guardduty/main.tf
+++ modules/guardduty/main.tf
@@ -1,10 +1,20 @@
+
+### 2. **`modules/guardduty/`** – AWS GuardDuty Module
+
+#### **main.tf**
+
+```hcl
 resource "aws_guardduty_detector" "main" {
   enable = true
 }
 
 resource "aws_guardduty_member" "member" {
-  for_each   = var.guardduty_accounts != {} ? var.guardduty_accounts : {}
+  for_each   = var.guardduty_accounts
   account_id = each.key
   email      = each.value
   invite     = true
 }
+
+
+
+
